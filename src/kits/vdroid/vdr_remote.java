f448764d151f3d4273b0c93f6dc7f3d99f26a698
Markus Keil <markuskeil@thereapman.net>
2010-07-25 11:24:41 +0000

    Add remote-source

---
+++ src/kits/vdroid/vdr_remote.java
@@ -0,0 +1,16 @@
+package kits.vdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class vdr_remote extends Activity {
+	
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.remote);   
+	}
+	
+
+}
