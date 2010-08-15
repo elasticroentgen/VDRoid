4c8b98dc53aa55b37c99a586d2c996df71c24e90
Markus Keil <thereapman@therreapman.net>
2010-08-15 09:48:20 +0000

    Added Source for Keypress

--- src/kits/vdroid/vdr_remote.java
+++ src/kits/vdroid/vdr_remote.java
@@ -2,6 +2,8 @@ package kits.vdroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class vdr_remote extends Activity {
 	
@@ -9,8 +11,167 @@ public class vdr_remote extends Activity {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.remote);   
+        setContentView(R.layout.remote);
+        
+        Button btn_0 = ((Button) findViewById(R.id.remo_0));
+        btn_0.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendKey("0");
+            }
+        });
+        
+        Button btn_1 = ((Button) findViewById(R.id.remo_1));
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendKey("1");
+            }
+        });
+        
+        Button btn_2 = ((Button) findViewById(R.id.remo_2));
+        btn_2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendKey("2");
+            }
+        });
+        
+        Button btn_3 = ((Button) findViewById(R.id.remo_3));
+        btn_3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendKey("3");
+            }
+        });
+        
+        Button btn_4 = ((Button) findViewById(R.id.remo_4));
+        btn_4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendKey("4");
+            }
+        });
+        
+        Button btn_5 = ((Button) findViewById(R.id.remo_5));
+        btn_5.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendKey("5");
+            }
+        });
+        
+        Button btn_6 = ((Button) findViewById(R.id.remo_6));
+        btn_6.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendKey("6");
+            }
+        });
+        
+        Button btn_7 = ((Button) findViewById(R.id.remo_7));
+        btn_7.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendKey("7");
+            }
+        });
+        
+        Button btn_8 = ((Button) findViewById(R.id.remo_8));
+        btn_8.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendKey("8");
+            }
+        });
+        
+        Button btn_9 = ((Button) findViewById(R.id.remo_9));
+        btn_9.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendKey("9");
+            }
+        });
+        
+        // COLORS
+        
+        Button btn_blue = ((Button) findViewById(R.id.remo_blue));
+        btn_blue.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendKey("Blue");
+            }
+        });
+        
+        Button btn_red = ((Button) findViewById(R.id.remo_red));
+        btn_red.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendKey("Red");
+            }
+        });
+        
+        Button btn_green = ((Button) findViewById(R.id.remo_green));
+        btn_green.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendKey("Green");
+            }
+        });
+        Button btn_yellow = ((Button) findViewById(R.id.remo_yellow));
+        btn_yellow.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendKey("Yellow");
+            }
+        });
+        
+        
+        //NAVIGATION
+        
+        Button btn_up = ((Button) findViewById(R.id.remo_up));
+        btn_up.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendKey("Up");
+            }
+        });
+        
+        Button btn_down = ((Button) findViewById(R.id.remo_down));
+        btn_down.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendKey("Down");
+            }
+        });
+        
+        Button btn_left = ((Button) findViewById(R.id.remo_left));
+        btn_left.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendKey("Left");
+            }
+        });
+        
+        Button btn_right = ((Button) findViewById(R.id.remo_right));
+        btn_right.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendKey("Right");
+            }
+        });
+        
+        Button btn_menu = ((Button) findViewById(R.id.remo_menu));
+        btn_menu.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendKey("Menu");
+            }
+        });
+        
+        Button btn_ok = ((Button) findViewById(R.id.remo_ok));
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendKey("Ok");
+            }
+        });
+        
+        Button btn_back = ((Button) findViewById(R.id.remo_exit));
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendKey("Back");
+            }
+        });
 	}
 	
 
+	private void sendKey(String key)
+	{
+		
+		
+	}
+	
+	
+	
 }
